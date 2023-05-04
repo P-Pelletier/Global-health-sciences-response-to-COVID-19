b6b758ff012e82cc69cddff826b094a0ee79aa06
Kevin Wirtz <kevin-w@hotmail.fr>
2023-05-04 12:38:02 +0200

    Update Clean_db.py

--- Downloads/Clean_db.py
+++ Downloads/Clean_db.py
@@ -7,11 +7,23 @@ from Downloads.utils import Clean_infos
 #parser.add_argument('-to_')
 #args = parser.parse_args()
 
-data = Clean_infos('pubmed','pubmed_2015','pubmed_2015_cleaned')
+data = Clean_infos('pubmed','all','pubmed_2015_cleaned')
 #data.update_db(int(args.from_),int(args.to_))
-data.update_db(int(0),int(11000000))
+data.update_db(int(30000000),int(35000000))
     
 
     
 data.restrict_medline()
 data.issn2categories()
+
+"""
+import pymongo
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client["pubmed"]
+collection = db["all"]
+
+
+doc = collection.find_one({"pmid":36869612})
+
+collection.delete_many({"meshwords":{"$exists":0}})
+"""
